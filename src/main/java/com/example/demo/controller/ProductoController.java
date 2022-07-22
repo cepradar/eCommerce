7ef1bcbb44ff6e60a0a8ac1665b7aac0b124d5cf
Carlos Prada <carlosprada95@gmail.com>
2022-07-22 09:52:01 -0500

    lista funcionalidad delete productos

--- src/main/java/com/example/demo/controller/ProductoController.java
+++ src/main/java/com/example/demo/controller/ProductoController.java
@@ -6,6 +6,7 @@ import org.slf4j.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -60,5 +61,11 @@ public class ProductoController {
     public String update(Producto producto){
         productoService.update(producto);
         return "redirect:/productos";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable Integer id){
+        productoService.delete(id);
+        return ("redirect:/productos");
     }
 }
