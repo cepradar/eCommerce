77538941b75ed83224aaa7e7272db92a873ac783
Carlos Prada <carlosprada95@gmail.com>
2022-07-22 12:31:54 -0500

    lista la vista para usuario normal

--- src/main/java/com/example/demo/controller/ProductoController.java
+++ src/main/java/com/example/demo/controller/ProductoController.java
@@ -1,20 +1,23 @@
 package com.example.demo.controller;
 
+import java.io.IOException;
 import java.util.Optional;
 
 import org.slf4j.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.example.demo.model.Producto;
 import com.example.demo.model.Usuario;
 import com.example.demo.service.ProductoService;
+import com.example.demo.service.UploadFileService;
 
 @Controller
 @RequestMapping("/productos")
@@ -25,6 +28,9 @@ public class ProductoController {
     @Autowired
     private ProductoService productoService;
 
+    @Autowired
+    private UploadFileService uFileService;
+
     @GetMapping("")
     public String show(Model model) {
         model.addAttribute("productos", productoService.findAll());
@@ -32,23 +38,31 @@ public class ProductoController {
     }
 
     @GetMapping("/create")
-    public String create(){
+    public String create() {
         return "productos/create";
     }
 
     @PostMapping("/save")
-    public String save(Producto producto){
+    public String save(Producto producto, @RequestParam("img") MultipartFile file) throws IOException {
         LOGGER.info("Este es el objeto producto{}", producto);
-        //u = usuario de prueba
+        // u = usuario de prueba
         Usuario u = new Usuario(1, "", "", "", "", "", "", "");
         producto.setUsuario(u);
+
+        // imagen
+        if (producto.getId() == null) { // cuando se crea un producto
+            String nombreImagen = uFileService.saveImage(file);
+            producto.setImagen(nombreImagen);
+        } else {
+
+        }
 
         productoService.save(producto);
         return "redirect:/productos";
     }
 
     @GetMapping("/edit/{id}")
-    public String edit(@PathVariable Integer id, Model model){
+    public String edit(@PathVariable Integer id, Model model) {
         Producto producto = new Producto();
         Optional<Producto> optionalProducto = productoService.get(id);
         producto = optionalProducto.get();
@@ -58,13 +72,39 @@ public class ProductoController {
     }
 
     @PostMapping("/update")
-    public String update(Producto producto){
+    public String update(Producto producto, @RequestParam("img") MultipartFile file) throws IOException {
+
+        Producto p = new Producto();
+        p = productoService.get(producto.getId()).get();
+
+        if (file.isEmpty()) {// editamos el producto pero no cambiamos la images
+
+            producto.setImagen(p.getImagen());
+        } else {// editamos el producto y se cambia la imagen
+            // eliminar cuando no sea la imagen por defecto
+            if (!p.getImagen().equals("default.jpg")) {
+                uFileService.deleteImage(p.getImagen());
+            }
+
+            String nombreImagen = uFileService.saveImage(file);
+            producto.setImagen(nombreImagen);
+        }
+        producto.setUsuario(p.getUsuario());
         productoService.update(producto);
         return "redirect:/productos";
     }
 
     @GetMapping("/delete/{id}")
-    public String delete(@PathVariable Integer id){
+    public String delete(@PathVariable Integer id) {
+
+        Producto p = new Producto();
+        p = productoService.get(id).get();
+
+        // eliminar cuando no sea la imagen por defecto
+        if (!p.getImagen().equals("default.jpg")) {
+            uFileService.deleteImage(p.getImagen());
+        }
+
         productoService.delete(id);
         return ("redirect:/productos");
     }
