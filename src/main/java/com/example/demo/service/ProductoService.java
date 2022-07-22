7ec06a4f739dc3e9790580806e57627d1a1d556f
Carlos Prada <carlosprada95@gmail.com>
2022-07-21 23:10:46 -0500

    listo mostrar productos y editar productos

--- src/main/java/com/example/demo/service/ProductoService.java
+++ src/main/java/com/example/demo/service/ProductoService.java
@@ -1,5 +1,6 @@
 package com.example.demo.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import com.example.demo.model.Producto;
@@ -9,5 +10,6 @@ public interface ProductoService {
     public Optional<Producto> get(Integer id);
     public void update(Producto producto);
     public void delete(Integer id);
+    public List<Producto> findAll();
     
 }
