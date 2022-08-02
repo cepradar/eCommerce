355b2a870d170db48fa0bd66dff3eec143807a5d
Carlos Prada <carlosprada95@gmail.com>
2022-08-02 14:01:07 -0500

    mostrar la vista compras para el user

--- src/main/java/com/example/demo/controller/UsuarioController.java
+++ src/main/java/com/example/demo/controller/UsuarioController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.slf4j.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -65,4 +66,12 @@ public class UsuarioController {
 
         return "redirect:/";
     }
+
+    @GetMapping("/compras")
+    public String compras(HttpSession session, Model model){
+        model.addAttribute("sesion", session.getAttribute("idUsuario"));
+        
+        return "usuario/compras";
+    }
+
 }
